c024a0f638dbb84b02d8c5b0fd21d4633f63cc46
Miron Pawlik <miron.pawlik@swmansion.com>
2017-12-12 15:44:38 +0100

    Add ios project Add implementation of getHourFormat for ios

---
+++ ios/RNHourFormat/RNHourFormat.h
@@ -0,0 +1,12 @@
+//
+//  RNHourFormat.h
+//  RNHourFormat
+//
+//  Copyright © 2017 Miron Pawlik. All rights reserved.
+//
+
+#import <React/RCTBridgeModule.h>
+
+@interface RNHourFormat : NSObject <RCTBridgeModule>
+
+@end
---
+++ ios/RNHourFormat/RNHourFormat.m
@@ -0,0 +1,60 @@
+//
+//  RNHourFormat.m
+//  RNHourFormat
+//
+//  Copyright © 2017 Miron Pawlik. All rights reserved.
+//
+
+#import "RNHourFormat.h"
+
+@interface RNHourFormat ()
+-(NSString*) getLocale;
+-(NSString*) getHourFormat;
+@end
+
+
+@implementation RNHourFormat
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE();
+
+- (NSString*) getLocale {
+    
+    NSArray *locales = [[NSUserDefaults standardUserDefaults] objectForKey:@"AppleLanguages"];
+    if (locales == nil) { return nil; }
+    if ([locales count] == 0) { return nil; }
+    
+    NSString* currentLocale = locales[0];
+    return currentLocale;
+}
+
+- (NSString*) getHourFormat {
+    // https://stackoverflow.com/a/11660380
+    NSString *formatStringForHours = [NSDateFormatter dateFormatFromTemplate:@"j" options:0 locale:[NSLocale currentLocale]];
+    
+    NSRange containsA = [formatStringForHours rangeOfString:@"a"];
+    if (containsA.location == NSNotFound) {
+        return @"24";
+    } else{
+        return @"12";
+    } ;
+}
+
+RCT_EXPORT_METHOD(getLocale:
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+    resolve([self getLocale]);
+}
+
+RCT_EXPORT_METHOD(getHourFormat:
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+resolve([self getHourFormat]);}
+
+
+@end
