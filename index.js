3ab04dd672e309dcbb841c4d8cbe0c84ac22fea4
Miron Pawlik <miron.pawlik@swmansion.com>
2017-12-13 12:27:44 +0100

    Add async methods to get correct values when app starts

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-import { NativeModules } from "react-native";
+import { NativeModules } from 'react-native';
 const { RNHourFormat } = NativeModules;
 
-export default class HourFormat {
-  static getLocale() {
+let locale = 'en-US';
+RNHourFormat.getLocale().then(result => (locale = result));
+
+let is24hourformat = false;
+RNHourFormat.getHourFormat().then(result => (is24hourformat = result === '24'));
+
+export class HourFormat {
+	static getLocale() {
+		return locale;
+	}
+
+	static is24HourFormat() {
+		return is24hourformat;
+	}
+}
+
+export class HourFormatAsync {
+  static getLocaleAsync() {
     return RNHourFormat.getLocale();
   }
 
-  static is24HourFormat() {
+  static is24HourFormatAsync() {
     return RNHourFormat.getHourFormat().then(result => result === "24");
   }
 }
